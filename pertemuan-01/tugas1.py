94c9cb7c8faf3b84a938196ed9c936713c900cb6
Aliya Yoan <aliyayoan11@email.com>
2026-09-03 21:24:36 +0700

    Tugas Pertemuan 1

---
+++ pertemuan-01/tugas1.py
@@ -0,0 +1,7 @@
+harga_awal = 200000
+persen_diskon = 15
+jumlah_diskon = (persen_diskon / 100) * harga_awal
+harga_akhir = harga_awal - jumlah_diskon
+print("Harga Awal : Rp", harga_awal)
+print("Besar Diskon : Rp", jumlah_diskon)
+print("Harga Akhir : Rp", harga_akhir) 
